40c568c101155cdee1050b6bfe21e885ad31ada8
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-10-04 19:31:54 +0200

    Still figuring out db connections 2

--- dash.py
+++ dash.py
@@ -3,9 +3,6 @@ from urlparse import urlparse
 from flask import Flask, render_template
 from pymongo import MongoClient
 
-# Create app
-# ------------------------------------------------------------------------
-app = Flask(__name__)
 
 # Setup DB stuff
 # ------------------------------------------------------------------------
@@ -22,6 +19,10 @@ else:
 	client = MongoClient('localhost', 27017)
 	db = client['runs_db']
 
+# Create app
+# ------------------------------------------------------------------------
+app = Flask(__name__)
+
 # Views
 # ------------------------------------------------------------------------
 @app.route('/')
@@ -30,18 +31,18 @@ def index():
 		cursor = db['runs_col'].find({}).sort("startTime", -1).limit(1)
 		return str(cursor[0]['_id'])
 	else:
-    	return 'No db connection: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
+		return 'No db connection: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
 
 @app.route('/user/')
 @app.route('/user/<username>')
 def show_user(username=None):
-    return render_template('page.html', name=username)
+	return render_template('page.html', name=username)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+	return render_template('404.html'), 404
 
 # Autostart
 # ------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
