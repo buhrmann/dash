56fd5d782a4fc967fdf29305f1c365cc9d5c3e57
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-10-04 19:26:06 +0200

    Still figuring out db connections

--- dash.py
+++ dash.py
@@ -3,21 +3,24 @@ from urlparse import urlparse
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+# Create app
+# ------------------------------------------------------------------------
+app = Flask(__name__)
+
 # Setup DB stuff
 # ------------------------------------------------------------------------
 MONGO_URL = os.environ.get('MONGOHQ_URL')
 if MONGO_URL:
 	# We're on the server, so get MongoHQ instance
+	print 'xxxxx: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
 	client = MongoClient(MONGO_URL)
+	db = client.get_default_database()
 	#db = client[urlparse(MONGO_URL).path[1:]]
+	print 'xxxxx: Established DB connection!'
 else:
 	# We're local, use localhost db coonection
 	client = MongoClient('localhost', 27017)
 	db = client['runs_db']
-
-# Start app
-# ------------------------------------------------------------------------
-app = Flask(__name__)
 
 # Views
 # ------------------------------------------------------------------------
@@ -27,7 +30,7 @@ def index():
 		cursor = db['runs_col'].find({}).sort("startTime", -1).limit(1)
 		return str(cursor[0]['_id'])
 	else:
-    	return 'No db connection: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:];
+    	return 'No db connection: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
 
 @app.route('/user/')
 @app.route('/user/<username>')
