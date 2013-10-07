afca954d979c63ad38e705e5a9229506ca403803
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-10-07 18:33:32 +0200

    Create empty runs collection if doesnt exist

--- dash.py
+++ dash.py
@@ -3,49 +3,62 @@ from urlparse import urlparse
 from flask import Flask, render_template
 from pymongo import MongoClient
 
+coll_name = "runs"	
 
-# Setup DB stuff
+# Connect to DB 
 # ------------------------------------------------------------------------
-MONGO_URL = os.environ.get('MONGOHQ_URL')
-if MONGO_URL:
-	# We're on the server, so get MongoHQ instance
-	print 'xxxxx: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
-	client = MongoClient(MONGO_URL)
-	db = client.get_default_database()
-	#db = client[urlparse(MONGO_URL).path[1:]]
-	print 'xxxxx: Established DB connection!'
-else:
-	# We're local, use localhost db coonection
-	client = MongoClient('localhost', 27017)
-	db = client['runs_db']
+def connect():
+	MONGO_URL = os.environ.get('MONGOHQ_URL')
 
-coll_name = "runs_col"	
+	if MONGO_URL:
+		# We're on the server, so get MongoHQ instance
+		client = MongoClient(MONGO_URL)
+		return client.get_default_database()
+		#db = client[urlparse(MONGO_URL).path[1:]]
+	else:
+		# We're local, use localhost db coonection
+		client = MongoClient('localhost', 27017)
+		return client['runs_db']
+
+# Get collection of runs
+# ------------------------------------------------------------------------
+def runs():
+	db = connect()
+	if(coll_name in db.collection_names()):
+		return db[coll_name]
+	else:
+		db.createCollection(coll_name)
+		return db[coll_name]
+
+runs = runs()		
 
 # Create app
 # ------------------------------------------------------------------------
 app = Flask(__name__)
 
+# ------------------------------------------------------------------------
 # Views
 # ------------------------------------------------------------------------
 @app.route('/')
 def index():
-	if db:
-		if(coll_name in db.collection_names()):
-			cursor = db['runs_col'].find({}).sort("startTime", -1).limit(1)
-			if cursor.count() > 0:
-				return str(cursor[0]['_id'])
-			else:
-				return 'Collection is empty!'
+	if not runs is None:
+		cursor = db['runs_col'].find({}).sort("startTime", -1).limit(1)
+		if cursor.count() > 0:
+			return str(cursor[0]['_id'])
 		else:
-			return 'Collection doesnt exist'
+			return 'Collection is empty!'
 	else:
-		return 'No db connection: ' + MONGO_URL + ' ' + urlparse(MONGO_URL).path[1:]
+		return 'Collection doesnt exist'
 
+# Views
+# ------------------------------------------------------------------------
 @app.route('/user/')
 @app.route('/user/<username>')
 def show_user(username=None):
 	return render_template('page.html', name=username)
 
+# Views
+# ------------------------------------------------------------------------
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
@@ -54,3 +67,4 @@ def page_not_found(e):
 # ------------------------------------------------------------------------
 if __name__ == '__main__':
 	app.run(debug=True)
+
---
+++ nike.py
@@ -0,0 +1,61 @@
+import json, httplib, urllib2
+import datetime
+import sys
+
+import nike
+
+accessToken = "860a6bdb7e7bd3ed0f70a7778cdf723c"
+baseurl = "https://api.nike.com/me/sport/activities/"
+
+def jsonFromUrl(url):
+	req = urllib2.Request(url)
+	req.add_header("appid", "fuelband")
+	req.add_header("Accept", "application/json")
+
+	try:
+		handle = urllib2.urlopen(req)
+	except IOError, e:
+		print "Wrong token or other connectivity problems!"
+		sys.exit(1)
+
+	js = json.load(handle)
+	return js
+
+# Converts activityId strings to integers
+def listNikeRuns(max=2, count=2):
+	print "Retrieving list of runs on nike+ server..."
+	ids = []
+	for i in range(0, max/count):
+		offset = i*count + 1
+		print "Downloading ids " + str(offset) + " to " + str(offset + count - 1)
+		url = baseurl + "?access_token=" + accessToken + "&count=" + str(count) + "&offset=" + str(offset)
+		js = jsonFromUrl(url)
+		idBatch = [int(act['activityId']) for act in js['data']]
+		#idBatch = [act['activityId'] for act in js['data']]
+		ids.extend(idBatch)
+		if(len(idBatch) < count):
+			break
+	return ids
+
+# Assumes runId is an integer. 
+# Returns a dictionary with run data that can be inserted into mongodb.
+def getNikeRun(runId):
+	# Retrieve gps data
+	print "Retrieving run " + str(runId) + " from nike server..."
+	url = baseurl + str(runId) + "/gps?access_token=" + accessToken
+	jsGps = jsonFromUrl(url)
+
+	run = {}
+	run["_id"] = runId
+	run["gps"] = jsGps["waypoints"]
+
+	# Retrieve meta data
+	url = baseurl + str(runId) + "?access_token=" + accessToken
+	jsMeta = jsonFromUrl(url)
+
+	startTimeStr = jsMeta["startTime"]
+	startDateTime = datetime.datetime.strptime(startTimeStr, "%Y-%m-%dT%H:%M:%SZ")
+	run["startTime"] = startDateTime
+
+	return run
+
