f7faa1aa5eadc68dfb5a66c39e1767e27a59878c
Thomas Buhrmann <thomas.buehrmann@gmail.com>
2013-11-03 23:56:41 +0100

    Nicer drop downs on analysis page.

--- static/js/d3scatter.js
+++ static/js/d3scatter.js
@@ -1,34 +1,48 @@
+var data;
 var vars = ["distance", "avgspeed", "duration", "maxspeed"];
 var xLabSel = vars[0];
 var yLabSel = vars[1];
 var zLabSel = vars[2];
 
+// Setup
+var include0 = false;
+var margin = {top: 10, right: 40, bottom: 100, left: 40};
+var width = 800 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
+
 //-------------------------------------------------------------------
 // Menu for variables with ith element selected by default
 //-------------------------------------------------------------------
-buildDropDown = function(varid){
+buildDropDown = function(varid, label){
 
-	d3.select(".varselector").append("select")
-		.attr("id", varid)
-	    .on("change", selectVars)
-		.selectAll("option")
-		.data(vars).enter().append("option")
-		    .attr("value", function(d){ return d; }) /* Optional */
-		    .attr("selected", function(d) { if (d==vars[varid]) return "selected";})
-		    .text(function(d){ return d; });
+	d3.select(".varselector").append("div")
+		.attr("class", "col-md-2")
+		.attr("style", "margin-bottom:20px;")
+		.text(label)
+		.append("div")
+			.attr("class", "select-style ")
+			.append("select")
+				.attr("id", varid)
+			    .on("change", selectVars)
+				.selectAll("option")
+				.data(vars).enter().append("option")
+				    .attr("value", function(d){ return d; }) /* Optional */
+				    .attr("selected", function(d) { if (d==vars[varid]) return "selected";})
+				    .text(function(d){ return runMapper[d]["label"]; });
 }
 
 //-------------------------------------------------------------------
 // Scatter plot with regression line
 //-------------------------------------------------------------------
-scatterFromJson = function(elem, data) {
+scatterFromJson = function(elem, dat) {
 
+	data = dat;
 	data.forEach(function(d){ d.date = dateFormat.parse(d.date); });
 	data.forEach(function(d){ d.duration = d.duration / 3600; });
 
-	buildDropDown(0);
-	buildDropDown(1);
-	buildDropDown(2);
+	buildDropDown(0, "x-Axis");
+	buildDropDown(1, "y-Axis");
+	buildDropDown(2, "Size");
 
 	scatter(elem, data, xLabSel, yLabSel, zLabSel);	
 }
@@ -46,6 +60,8 @@ selectVars = function(){
 		zLabSel = this.options[this.selectedIndex].value;
 
 	console.log(xLabSel, yLabSel, zLabSel);
+
+	updateScatter()
 }
 
 //-------------------------------------------------------------------
@@ -133,10 +149,23 @@ linearRegression = function(dat, xlab, ylab) {
 //-------------------------------------------------------------------
 // Draw line from intercept and slope
 //-------------------------------------------------------------------
-drawline = function(elem, factors, x1, x2, xscale, yscale){
+drawline = function(factors, x1, x2, xscale, yscale){
 
   	//console.log(factors);
-	line = elem.append("line")
+	//line = d3.select("svg").select("g").append("line")
+	line = d3.select("svg").select("g").selectAll(".line").data([{"dummy":1}]);
+	
+	// Update existing line
+	line.attr(
+	    {
+	        "x1" : xscale(x1),
+	        "x2" : xscale(x2),
+	        "y1" : yscale(factors[0]),
+	        "y2" : yscale(factors[0] + factors[1]*x2),
+    });
+
+	// Create if it doesn't exist
+	line.enter().append("line")
 	    .attr(
 	    {
 	    	"class" : "line",
@@ -152,16 +181,46 @@ drawline = function(elem, factors, x1, x2, xscale, yscale){
 }
 
 //-------------------------------------------------------------------
-// Scatter chart
+// Update scatter chart
+// In this case the data is not actually changing but only the axes,
+// and therefore the position of dots in the chart
 //-------------------------------------------------------------------
-scatter = function(id, dat, xlab, ylab, zlab) {
-
-	// Setup
-	var margin = {top: 10, right: 40, bottom: 100, left: 40};
-	var width = 800 - margin.left - margin.right;
-	var height = 500 - margin.top - margin.bottom;
+updateScatter = function() {
 	
-	include0 = false;
+	xdomain = [0, d3.max(data, function(d) { return d[xLabSel]; })];
+	var x = d3.scale.linear().domain(xdomain).nice().rangeRound([0, width]); 
+	var xAxis = d3.svg.axis().scale(x).orient("bottom")
+
+	if (include0)
+		ydomain = [0, d3.max(data, function(d) { return d[yLabSel]; })];
+	else
+		ydomain = d3.extent(data, function(d) { return d[yLabSel]; });
+	var y = d3.scale.linear().domain(ydomain).nice().range([height, 0]);
+	var yAxis = d3.svg.axis().scale(y).orient("left");
+
+	zdomain = d3.extent(data, function(d) { return d[zLabSel]; });
+	var z = d3.scale.linear().domain(zdomain).range([2,10]);
+
+	// update existing	
+	var dots = d3.selectAll(".dot")
+		.transition().duration(200)
+		.attr("cx", function(d) { return x(d[xLabSel]); })
+		.attr("cy", function(d) { return y(d[yLabSel]); })
+		.attr("r", function(d) { return z(d[zLabSel]); });
+
+	d3.select(".x.axis").call(xAxis)
+		.select(".axisLabel")
+		.text(runMapper[xLabSel]["label"]);
+
+	d3.select(".y.axis").call(yAxis)
+		.select(".axisLabel")
+		.text(runMapper[yLabSel]["label"]);;
+
+	factors = linearRegression(data, xLabSel, yLabSel);
+	drawline(factors, 0, xdomain[1], x, y);
+}
+
+scatter = function(id, dat, xlab, ylab, zlab) {
 
 	// Create scales and axis
 	xdomain = [0, d3.max(dat, function(d) { return d[xlab]; })];
@@ -196,6 +255,7 @@ scatter = function(id, dat, xlab, ylab, zlab) {
 		.append("text")
 		  .attr("y", -8)
 		  .attr("x", width)
+		  .attr("class", "axisLabel")
 		  .style("text-anchor", "end")
 		  .text(runMapper[xlab]["label"]);
 
@@ -206,6 +266,7 @@ scatter = function(id, dat, xlab, ylab, zlab) {
 		  .attr("transform", "rotate(-90)")
 		  .attr("y", 6)
 		  .attr("dy", ".71em")
+		  .attr("class", "axisLabel")
 		  .style("text-anchor", "end")
 		  .text(runMapper[ylab]["label"]);
 
@@ -217,7 +278,7 @@ scatter = function(id, dat, xlab, ylab, zlab) {
 	svg.call(tip);
 
 	// Create marks
-	var bars = svg.selectAll(".dots")
+	var dots = svg.selectAll(".dot")
 		.data(dat, function(d) { return d[xlab]; })
 		.enter().append("svg:circle")
 		.on('mouseover', function(d) { tip.show(d); } )
@@ -229,6 +290,6 @@ scatter = function(id, dat, xlab, ylab, zlab) {
 
 	// Draw regression line
 	factors = linearRegression(dat, xlab, ylab);
-	drawline(svg, factors, 0, xdomain[1], x, y);
+	drawline(factors, 0, xdomain[1], x, y);
 }
 	
